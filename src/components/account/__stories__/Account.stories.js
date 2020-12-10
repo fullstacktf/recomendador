3b7683afcd35749ca48ccc4d5639d31bc2cf3fa6
Yodra López <yodralopez@gmail.com>
2020-12-10 20:29:32 +0000

    scafolding recomendator

---
+++ src/components/account/__stories__/Account.stories.js
@@ -0,0 +1,16 @@
+import Account from "../Account.vue";
+
+export default {
+    title: 'Account',
+    component: Account,
+    argType: {
+        backgroundColor: {control: 'green'}
+    }
+}
+
+const Template = () => ({
+    components: {Account},
+    template: '<Account/>'
+});
+export const ByDefault = Template.bind({});
+ByDefault.args = {};
---
+++ src/components/counter/__stories__/Counter.stories.js
@@ -0,0 +1,16 @@
+import Counter from '../Counter.vue';
+
+export default {
+  title: 'Counter',
+  component: Counter,
+  argTypes: {}
+};
+
+const Template = (args, { argTypes }) => ({
+  props: Object.keys(argTypes),
+  components: { Counter },
+  template: '<Counter/>'
+});
+
+export const ByDefault = Template.bind({});
+ByDefault.args = {};
