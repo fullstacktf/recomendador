31eeeff81296e3478a8db0a855654f79d2e54e0e
Alejandro Suarez <asuagar94@gmail.com>
2020-12-12 21:54:10 +0000

    create views and slide components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import "./assets/tailwind.css";
+import router from "./router";
+import store from "./store";
+import VueGlide from "vue-glide-js";
+import "vue-glide-js/dist/vue-glide.css";
+
+Vue.use(VueGlide);
 
 Vue.config.productionTip = false;
 
 new Vue({
+  router,
+  store,
   render: h => h(App)
 }).$mount("#app");
